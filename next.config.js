1a0cf9a7e7769e84e0e574db11b1c5388a7e8af0
reuben-roy <reubenroyk@gmail.com>
2026-04-25 12:11:22 -0700

    fix: merge duplicate next.config files into single config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
+    domains: ['cms.explosion.fun'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cms.explosion.fun',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   trailingSlash: true,
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
